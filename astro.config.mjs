09146bdafaf0a1fb21cf092117bd8f0d9e877d78
Mo Atie <mhd.atie.ca@gmail.com>
2025-12-15 22:14:48 -0600

    change example.com to moatie.dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import cloudflare from '@astrojs/cloudflare';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import cloudflare from "@astrojs/cloudflare";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	adapter: cloudflare(),
-	integrations: [mdx(), sitemap()],
+  site: "https://moatie.dev",
+  adapter: cloudflare(),
+  integrations: [mdx(), sitemap()],
 });
